afaf0581ec7e10d0d7b8b11489d42151e92940db
Connor Phillips <connordphillips@gmail.com>
2023-07-19 20:19:40 -0400

    adding header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
+import Header from './header';
+import Footer from './footer';
 
 export default function Home() {
   return (
     <main>
-      <div className='bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl'>
-        <div>Test</div>
+      <div className='container'>
+        <Header />
+        <div className='bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl'>
+          <div>Test</div>
+        </div>
+        <Footer />
       </div>
     </main>
   );
