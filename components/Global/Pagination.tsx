3383e3387cdd959c60bb1be1f3dd641d3129889c
Connor Phillips <connordphillips@gmail.com>
2023-10-26 20:00:48 -0400

    fixing pagination

--- components/Global/Pagination.tsx
+++ components/Global/Pagination.tsx
@@ -15,16 +15,18 @@ export default function Pagination({
   prevPage,
 }: PaginationProps) {
   return (
-    <div className='flex flex-row space-x-10 py-10'>
+    <div className='flex flex-row py-10'>
+      <ul className='flex flex-row px-5'>
       {prevPage && prevPage === 1 ? (
-        <Link href='/'>Previous Page</Link>
+        <li className='px-2'><Link href='/' className='rounded-full border-2 border-midnight-500 hover:bg-midnight-500 hover:text-white p-2'>Previous Page</Link></li>
       ) : prevPage ? (
-        <Link href={prevPage?.toString()}>Previous Page</Link>
+        <li className='px-2'><Link href={prevPage?.toString()}>Previous Page</Link></li>
       ) : null}
-      <p>
+      <p className='px-2'>
         Page {pageNum?.toString()} of {pageTot?.toString()}
       </p>
-      {nextPage ? <Link href={`/page/${nextPage?.toString()}`}>Next Page</Link> : null}
+      {nextPage ? <li className='px-2'><Link href={`/page/${nextPage?.toString()}`} className='rounded-full border-2 border-midnight-500 hover:bg-midnight-500 hover:text-white p-2'>Next Page</Link></li> : null}
+      </ul>
     </div>
   );
 }
